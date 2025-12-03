7781fec8238d96523ecfb0ec31e1b85cbe2a4dc6
Eda Çalışkan <edacaliskan4402@gmail.com>
2025-12-03 22:44:24 +0300

    Update on the placement of the functions.py Update in the dependencies.py and auth.py

--- Project/app/dependencies.py
+++ Project/app/dependencies.py
@@ -1,13 +1,19 @@
+from typing import Optional
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from .database import SessionLocal
-#import jwt
+from .models import User
+from .schemas import Token
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 SECRET_KEY = "your-super-secret-key-change-in-production"
 ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def get_db():
     db = SessionLocal()
@@ -16,3 +22,35 @@ def get_db():
     finally:
         db.close()
 
+def create_acces_token(data:dict,expires_delta:Optional[timedelta]=None):
+    to_encode=data.copy()
+    if expires_delta:
+        expire=datetime.now(timezone.utc)+expires_delta
+    else:
+        expire=datetime(timezone.utc)+timedelta(minutes=15)
+    to_encode.update({"exp":expire})
+    encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(db:Session=Depends(get_db),token:str=Depends(oauth2_scheme)):
+    credential_expectations=HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username: str=payload.get("sub")
+        if username is None:
+            raise credential_expectations
+    except JWTError:
+        raise credential_expectations
+    user=db.querry(User).filter(User.userName==username).first()
+    if user is None:
+        raise credential_expectations
+    return user
+
+async def get_current_admin(current_user: User=Depends(get_current_user)):
+    if not get_current_user.is_admin:
+        raise HTTPException(status_code=403, detail="Not enough permissions")
+    return current_user
